1427ae8a9495afc07f683222a249d5b7a56de1b4
Mgokayay <aymustafagokay@gmail.com>
2024-05-08 14:37:05 +0300

    plant exception created

---
+++ src/main/java/com/example/spring6/exceptions/PlantException.java
@@ -0,0 +1,17 @@
+package com.example.spring6.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@AllArgsConstructor
+@Data
+public class PlantException extends RuntimeException{
+
+    private HttpStatus httpStatus;
+
+    public PlantException(String message,HttpStatus httpStatus){
+        super(message);
+        this.httpStatus=httpStatus;
+    }
+}
